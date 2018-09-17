2d7fa2351500398a45f5b639d272008316abc0d3
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-17 18:05:25 -0500

    routes

---
+++ public/Javascript/app.js
@@ -0,0 +1,20 @@
+function displayResults(scrapedData) {
+  // First, empty the table
+  $("tbody").empty();
+
+  // Then, for each entry of that json...
+  scrapedData.forEach(function(test) {
+    // Append each of the animal's properties to the table
+    var tr = $("<tr>").append(
+      $("<td>").text(test.title),
+      $("<td>").text(test.link)
+    );
+
+    $("tbody").append(tr);
+  });
+}
+
+$.getJSON("/all", function(data) {
+  // Call our function to generate a table body
+  displayResults(data);
+});
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+// var bodyParser = require("body-parser");
+// var exphbs = require("express-handlebars");
 // Require request and cheerio. This makes the scraping possible
 var request = require("request");
 var cheerio = require("cheerio");
@@ -13,11 +13,11 @@ var cheerio = require("cheerio");
 
 // Initialize Express
 var app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 // Database configuration
 var databaseUrl = "scraper";
@@ -32,4 +32,53 @@ db.on("error", function(error) {
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
   res.send("Hello world");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  db.scrapedData.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+// Scrape data and place into monogodb db
+app.get("/scrape", function(req, res) {
+  request("https://old.reddit.com/r/Fitness/", function(error, reponse, html) {
+    var $ = cheerio.load(html);
+
+    $(".title").each(function(i, element) {
+      var title = $(this)
+        .children("a")
+        .text();
+      var link = $(this)
+        .children("a")
+        .attr("href");
+
+      if (title && link) {
+        db.scrapedData.insert(
+          {
+            title: title,
+            link: link
+          },
+          function(err, inserted) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(inserted);
+            }
+          }
+        );
+      }
+    });
+  });
+  res.send("Scrape Complete");
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
